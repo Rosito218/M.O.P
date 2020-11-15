6ea430cbcb15d76551230ba3687c4b2ff1b2f6d5
Rosito218 <74488885+Rosito218@users.noreply.github.com>
2020-11-15 13:28:27 +0400

    Add files via upload

---
+++ M.O.P/MOPS.py
@@ -0,0 +1,21 @@
+﻿from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import win32api, win32con
+
+while keyboard.is_pressed('q') != True:
+    if pyautogui.locateOnScreen("his and civ.png")!= None:
+        pyautogui.click('his and civ.png')
+    if pyautogui.locateOnScreen("Join.png")!= None:
+        pyautogui.click('Join.png')
+    if pyautogui.locateOnScreen("Join2.png")!= None:
+        pyautogui.click('Join2.png')
+    if pyautogui.locateOnScreen("Full_screen.png")!= None:
+        pyautogui.click('Full_screen.png')
+    if pyautogui.locateOnScreen('leave.png')!= None:
+        time.sleep(3600)
+        pyautogui.click('leave.png')
+        
+        
+   
